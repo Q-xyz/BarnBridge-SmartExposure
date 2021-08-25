1391a0c012fd9fa04648a333ca71732f021e24bc
jo-es <45110941+jo-es@users.noreply.github.com>
2021-08-25 15:21:46 +0200

    Fix unit in gasPrice calc. for polygon

--- tasks/contracts/EPoolPeriphery/rebalanceWithFlashSwap.ts
+++ tasks/contracts/EPoolPeriphery/rebalanceWithFlashSwap.ts
@@ -4,7 +4,7 @@ import fetch from 'node-fetch';
 
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
-import { EPool, EPoolHelper, EPoolPeriphery, TestERC20 } from '../../../typechain';
+import { EPool, EPoolHelper, EPoolPeriphery } from '../../../typechain';
 
 import { REBALANCE_WITH_FLASH_SWAP } from '../../task-names';
 
@@ -16,17 +16,18 @@ task(REBALANCE_WITH_FLASH_SWAP, 'Rebalances a EPool with a flash swap')
 .setAction(async function (_taskArgs: any, hre) {
   const { ethers, artifacts } = hre;
   const admin: Signer = (await ethers.getSigners())[0];
+  const chainId = Number((await ethers.provider.getNetwork()).chainId);
 
   const ePool = new ethers.Contract(_taskArgs.ePool, artifacts.readArtifactSync('EPool').abi) as EPool;
   const ePoolHelper = new ethers.Contract(_taskArgs.ePoolHelper, artifacts.readArtifactSync('EPoolHelper').abi) as EPoolHelper;
   const ePoolPeriphery = new ethers.Contract(_taskArgs.ePoolPeriphery, artifacts.readArtifactSync('EPoolPeriphery').abi) as EPoolPeriphery;
   const rate = await ePool.connect(admin).getRate();
-  const tokenA = new ethers.Contract(await ePool.connect(admin).tokenA(), artifacts.readArtifactSync('TestERC20').abi) as TestERC20;
-  const tokenB = new ethers.Contract(await ePool.connect(admin).tokenB(), artifacts.readArtifactSync('TestERC20').abi) as TestERC20;
-  const balanceA = await tokenA.connect(admin).balanceOf(await admin.getAddress());
-  const balanceB = await tokenB.connect(admin).balanceOf(await admin.getAddress());
-  console.log(`balanceA:         ${balanceA.toString()}`);
-  console.log(`balanceB:         ${balanceB.toString()}`);
+  // const tokenA = new ethers.Contract(await ePool.connect(admin).tokenA(), artifacts.readArtifactSync('TestERC20').abi) as TestERC20;
+  // const tokenB = new ethers.Contract(await ePool.connect(admin).tokenB(), artifacts.readArtifactSync('TestERC20').abi) as TestERC20;
+  // const balanceA = await tokenA.connect(admin).balanceOf(await admin.getAddress());
+  // const balanceB = await tokenB.connect(admin).balanceOf(await admin.getAddress());
+  // console.log(`balanceA:         ${balanceA.toString()}`);
+  // console.log(`balanceB:         ${balanceB.toString()}`);
   const [deltaA, deltaB, rChange, rDiv] = await ePoolHelper.connect(admin).delta(_taskArgs.ePool);
   console.log(`Estimated Rebalance:`);
   console.log(`  rDiv:           ${rDiv.toString()}`);
@@ -40,7 +41,7 @@ task(REBALANCE_WITH_FLASH_SWAP, 'Rebalances a EPool with a flash swap')
   const currentTimestamp = ethers.BigNumber.from(Math.round(new Date().getTime() / 1000));
   const nextRebalanceTimestamp = lastRebalance.add(rebalanceInterval);
   if (deltaA.eq(0) || deltaB.eq(0)) {
-    console.log(`Delt is zero. Nothing to rebalance.`);
+    console.log(`Delta is zero. Nothing to rebalance.`);
     return;
   }
   if (rDiv.lt(rebalanceMinRDiv)) {
@@ -54,25 +55,37 @@ task(REBALANCE_WITH_FLASH_SWAP, 'Rebalances a EPool with a flash swap')
 
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
-  let gasPrice = '1';
-  if (Number((await ethers.provider.getNetwork()).chainId) === 137) {
-    gasPrice = String((await (await fetch('https://gasstation-mainnet.matic.network/')).json()).fast);
-  } else {
-    gasPrice = String((await (await fetch('https://www.gasnow.org/api/v3/gas/price')).json()).data.fast);
+  let gasPrice = ethers.utils.parseUnits('1', 'gwei');
+  if (chainId === 1) {
+    gasPrice = ethers.utils.parseUnits(
+      String((await (await fetch('https://www.gasnow.org/api/v3/gas/price')).json()).data.fast),
+      'wei'
+    );
+  } else if (chainId === 137) {
+    gasPrice = ethers.utils.parseUnits(
+      String((await (await fetch('https://gasstation-mainnet.matic.network/')).json()).fastest),
+      'gwei'
+    );
+  }
+  if (gasPrice.gte(ethers.utils.parseUnits('300', 'gwei'))) {
+    console.log(`Exceeds gasPrice limit of 300 gwei. GasPrice ${ethers.utils.formatUnits(gasPrice, 'gwei')} gwei.`);
+    return;
   }
   try {
     await ePoolPeriphery.connect(admin).callStatic.rebalanceWithFlashSwap(
       ePool.address, ethers.utils.parseUnits('1', 18)
     );
   } catch (error) {
-    console.log('Gas estimation failed: ' + error);
+    console.log('Dry run failed: ' + error);
     return;
   }
+
   const tx_rebalance = await ePoolPeriphery.connect(admin).rebalanceWithFlashSwap(
-    ePool.address, ethers.utils.parseUnits('1', 18), { gasLimit: 500000, gasPrice: gasPrice }
+    ePool.address, ethers.utils.parseUnits('1', 18), { gasLimit: 500000, gasPrice }
   );
   console.log(`EPoolPeriphery.rebalanceAllWithFlashSwap:`);
   console.log(`  TxHash:         ${tx_rebalance.hash}`);
+  console.log(`  GasPrice:       ${ethers.utils.formatUnits(gasPrice, 'gwei')} gwei`);
   const receipt = await tx_rebalance.wait();
   const RebalanceEvent = new ethers.utils.Interface([ePool.interface.getEvent('RebalancedTranches')]);
   receipt.events?.forEach((event: any) => {
